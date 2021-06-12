dbdb7d531cf1da23667ad83786f1178f0a00c499
SpiriT-L <m.leon-id@yandex.ru>
2021-06-12 23:49:43 +0300

    feat: add htmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,27 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const isDev =process.env.NODE_ENV === 'development';
+const isDev = process.env.NODE_ENV === 'development';
 const isProd = !isDev;
 
-const filename = (ext) => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
+const filename = (ext) =>
+  isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
 
-module.exports ={
+module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
   entry: './js/main.js',
   output: {
     filename: `./js/${filename('js')}`,
     path: path.resolve(__dirname, 'app'),
-  }
-}
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      filename: 'index.html',
+      minify: {
+        collapseWhitespace: isProd,
+      },
+    }),
+  ],
+};

3dad925b4fde77d2a1e15d504d7ebccac5620f59
SpiriT-L <m.leon-id@yandex.ru>
2021-06-13 00:07:47 +0300

    feat: add CleanWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const isDev = process.env.NODE_ENV === 'development';
 const isProd = !isDev;
@@ -23,5 +24,6 @@ module.exports = {
         collapseWhitespace: isProd,
       },
     }),
+    new CleanWebpackPlugin(),
   ],
 };
